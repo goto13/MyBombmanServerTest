e2278c94d190911d3b807b76d0d1e841f51ce587
goto13 <gtmskaai@gmail.com>
2015-07-14 01:52:38 +0900

    log by log4j2

---
+++ MyTest.java
@@ -0,0 +1,20 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by USER1 on 2015/07/14.
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger();
+        logger.trace("TraceMessage");
+        logger.debug("DebugMessage");
+        logger.info("InfoMessage {}!", "Hello Log4j2");
+        logger.warn("WarnMessage {}!", "Hello Log4j2");
+        logger.error("ErrorMessage");
+        logger.fatal("FatalMessage");
+
+        logger.info("Exception", new IllegalArgumentException("Oops!"));
+    }
+}
